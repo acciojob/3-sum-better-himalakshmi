b5412b3baede16e3746f00e94923d03a51264287
himalakshmi <124651043+himalakshmi@users.noreply.github.com>
2023-06-08 10:34:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-function threeSum(arr, target) {
-// write your code here
-  arr.sort((a,b)=>a-b);
-	let ans = arr[0]+arr[1]+arr[2];
-	for(let i=0;i<arr.length-2;i++){
-		let start = i+1, end = arr.length-1;
-		while(start<end){
-			const sum = arr[i]+arr[start]+arr[end];
-			if(sum>target){
-				end--;
-			}else{
-				start++;
-			}
-			if(Math.abs(target-sum)<Math.abs(target-ans)){
-				ans=sum;
-			}
-		}
-	}
+unction threeSum(S, target) {
+  // Sort the array in ascending order
+  S.sort((a, b) => a - b);
+
+  // Initialize variables to store the closest sum and the minimum difference
+  let closestSum = Infinity;
+  let minDiff = Infinity;
+
+  // Iterate through the array
+  for (let i = 0; i < S.length - 2; i++) {
+    // Use two pointers to find the remaining two numbers
+    let left = i + 1;
+    let right = S.length - 1;
+
+    while (left < right) {
+      const sum = S[i] + S[left] + S[right];
+      const diff = Math.abs(sum - target);
+
+      // Update the closest sum and minimum difference if necessary
+      if (diff < minDiff) {
+        minDiff = diff;
+        closestSum = sum;
+      }
+
+      if (sum < target) {
+        // Increase the left pointer to increase the sum
+        left++;
+      } else {
+        // Decrease the right pointer to decrease the sum
+        right--;
+      }
+    }
+  }
+
+  return closestSum;
+}
